fb31bf84738ec65cce928a5a74b36be546f64b36
reisuj <reisuj@gmail.com>
2019-10-15 20:51:29 -0400

    Partway through first section of the Survival Guide

---
+++ Assets/VariablesSection/Scripts/Item.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Item : MonoBehaviour
+{
+    public string itemName = "Sword";
+    public string description = "Long bladed weapon";
+    public Sprite sprite;
+    public int attStrength = 100;
+    public int goldCost = 10;
+    public int durability = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log(itemName + ": " + description);
+        Debug.Log("Attack Strngth: " + attStrength);
+        Debug.Log("Cost: " + goldCost);
+        Debug.Log("Durability: " + durability);
+    }
+
+    
+}
---
+++ Assets/VariablesSection/Scripts/Player.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public string myName = "Brian";
+    public int myAge = 49;
+    public float mySpeed = 18;
+    public int myHealth = 18;
+    public int score = 20;
+    public float speed = 18;
+    public bool areAllKeysCollected = false;
+    public int ammo = 100;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        Debug.Log("My name is " + myName + " and I am " + myAge + " years old. I have a speed of " + mySpeed + " and my health is " + myHealth + ".");
+        Debug.Log("My score is " + score + " and the speed of " + speed + ". I have " + ammo + " and my collected keys is " + areAllKeysCollected + ".");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
